974ca147bd6e81b5426ba1c72d3396dfe26500c8
matrix <lhrmachinery@163.com>
2021-07-07 21:00:37 +0800

    Modify reorg script

--- bchnode/scripts/reorg.sh
+++ bchnode/scripts/reorg.sh
@@ -1,2 +1,2 @@
 set -eux
-curl -X POST --data "{\"method\":\"reorg\",\"params\":1,\"id\":1}"  -H "Content-Type: text/plain" http://localhost:1234
+curl -X POST --data "{\"method\":\"reorg\",\"params\":[1],\"id\":1}"  -H "Content-Type: text/plain" http://localhost:1234
