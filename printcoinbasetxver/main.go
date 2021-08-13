0f69379e4c41a72f386dce96f461dab11c28436f
zxh0 <zhangxiuhong2013@gmail.com>
2021-08-13 08:56:15 +0800

    Add printcoinbasetxver/main.go

---
+++ printcoinbasetxver/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tendermint/tendermint/libs/log"
+	"os"
+
+	"github.com/smartbch/smartbch/staking"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		fmt.Printf("Usage: %s <rpc-url> <username> <password>\n", os.Args[0])
+		return
+	}
+
+	rpcURL := os.Args[1]
+	username := os.Args[2]
+	password := os.Args[3]
+
+	client := staking.NewRpcClient(rpcURL, username, password, "text/plain;",
+		log.NewNopLogger())
+	h := client.GetLatestHeight()
+	fmt.Println("latest height:", h)
+
+	for ; h > 0; h-- {
+		bHash, err := client.GetBlockHash(h)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		bInfo, err := client.GetBlockInfo(bHash)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		cbTx, err := client.GetTxInfo(bInfo.Tx[0])
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		fmt.Printf("block: %d, coinbase tx version: %d\n", h, cbTx.Version)
+	}
+}
